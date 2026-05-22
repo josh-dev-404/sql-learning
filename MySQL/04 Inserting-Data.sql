6951fd2fd1d032ece28dccfc6abe31945130e25a
Josh-devx-404 <joshdevx40@gmail.com>
2026-05-22 21:43:23 +0800

    SQL COURSE COMPLETED

---
+++ MySQL/04 Inserting-Data.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE dbSchool;
+Use dbSchool;
+
+Create table tblStudents (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE,
+    email VARCHAR(100) UNIQUE
+);
+
+--Inserting Data from the table "tblStudents"
+-- syntax: INSERT INTO table_name (column1, column2, ...) VALUES (value1, value2, ...);
+
+INSERT INTO tblStudents
+(student_id, first_name, last_name, date_of_birth, email) values
+(1,"Josh","devx", "2005-01-29", "josh@gmail.com"),
+(2,"J","404", "2005-01-21", "james@gmail.com");
