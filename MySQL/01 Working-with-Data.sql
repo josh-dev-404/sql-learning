07b66869a425bbc367acef1faee88a6f5fdec915
Josh-devx-404 <joshdevx40@gmail.com>
2026-05-22 20:43:58 +0800

    mySQL

---
+++ MySQL/01 Working-with-Data.sql
@@ -0,0 +1,11 @@
+
+-- syntax for selecting all columns from a table;
+SELECT column_name
+FROM table_name;
+
+-- select data from a table;
+SELECT title from movies;
+
+-- select multiple columns;
+SELECT title, release_year from movies;
+
