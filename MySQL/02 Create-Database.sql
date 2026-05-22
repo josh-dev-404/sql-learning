0af144e545bf7ac504096c07ed56a542e03c5a8b
Josh-devx-404 <joshdevx40@gmail.com>
2026-05-22 21:25:12 +0800

    SQL COURSE COMPLETED

---
+++ MySQL/02 Create-Database.sql
@@ -0,0 +1,4 @@
+
+-- Create a database named dbSchool
+-- syntax: CREATE DATABASE database_name;
+CREATE DATABASE dbSchool;
---
+++ MySQL/03 Create-Table.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE dbSchool;
+Use dbSchool;
+
+-- Create a table named tblStudents
+-- syntax: CREATE TABLE table_name (column1 datatype, column2 datatype, ...);
+Create table tblStudents (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE,
+    email VARCHAR(100) UNIQUE
+);
