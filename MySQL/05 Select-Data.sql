da7ac0c1545e121d1eb38a815b538c59661e7370
Josh-devx-404 <joshdevx40@gmail.com>
2026-05-23 22:10:58 +0800

    SQL COURSE COMPLETED

---
+++ MySQL/05 Select-Data.sql
@@ -0,0 +1,20 @@
+
+
+Create table tblStudents (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE,
+    email VARCHAR(100) UNIQUE
+);
+
+INSERT INTO tblStudents (student_id,first_name,last_name,date_of_birth,email) values
+(1,"Josh","devx", "2005-01-29", "josh@gmail.com"),
+(2,"J","404", "2005-01-21", "james@gmail.com");
+
+
+-- Select all data from the table "tblStudents"
+SELECT * FROM tblStudents;
+
+-- Select specific columns from the table "tblStudents"
+SELECT first_name, last_name FROM tblStudents;
