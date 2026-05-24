04ffa322d3982ad4ec976d209062e98991c58b3d
Josh-devx-404 <joshdevx40@gmail.com>
2026-05-24 23:27:40 +0800

    SQL COURSE COMPLETED

--- MySQL/01 Basics/02 Create-Database.sql
+++ MySQL/01 Basics/02 Create-Database.sql
@@ -1,4 +1,11 @@
-
 -- Create a database named dbSchool
 -- syntax: CREATE DATABASE database_name;
 CREATE DATABASE dbSchool;
+
+-- Use the created database
+-- syntax: USE database_name;
+USE dbSchool;
+
+-- Drop database environment
+-- syntax: DROP DATABASE database_name;
+DROP DATABASE dbSchool;
