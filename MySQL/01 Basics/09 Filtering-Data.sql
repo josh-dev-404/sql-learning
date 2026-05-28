793d1c167fe0d42ea2cbb99eeed3ca1b1c8642f0
Josh-devx-404 <joshdevx40@gmail.com>
2026-05-28 21:26:39 +0800

    SQL COURSE COMPLETED

---
+++ MySQL/01 Basics/09 Filtering-Data.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE dbProduct;
+USE dbProduct;
+
+    CREATE TABLE tblProduct (
+    product_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_name VARCHAR(50) NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10,2) NOT NULL
+    );
+    
+    INSERT INTO tblProduct (product_name, quantity, price) VALUES
+    ("Ballpen", 20, 10),
+    ("Notebook", 30, 20),
+    ("Tape", 24, 17);
+    
+SELECT * FROM tblProduct WHERE product_name = "Ballpen";
+-- output:
+-- product_id | product_name | quantity | price
+-- 1          | Ballpen      | 20       | 10.00
+SELECT product_name, price FROM tblProduct WHERE price > 15;
+-- output:
+-- product_id | product_name | quantity | price
